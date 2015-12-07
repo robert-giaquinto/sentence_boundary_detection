Fixed some bugs in the algorithm. Modified the hidden layer to use softmax since I only plan on doing classification with this algorithm. Using TanH activiation funcation as default since it seems to converge faster.

Layers.py
@@ -4,7 +4,6 @@ from helper_funcs import *
 
 
 class Base_Layer(object):
-
 	def __init__(self, activation, d_activation):
 		self.activation = activation
 		self.d_activation = d_activation
@@ -17,50 +16,61 @@ class Base_Layer(object):
 
 
 class Input_Layer(object):
-	def __init__(self, n_in, n_out, activation, d_activation, next_layer, alpha=0.1, lambda_penalty=0.0001, random_state=None):
+	def __init__(self, num_inputs, num_outputs, activation, d_activation, next_layer,
+			alpha=0.1, lambda_penalty=0.0001, random_state=None):
+		# initialize weights and biases based on deep learning book recommendation
 		self.W = 4 * np.asarray(random_state.uniform(
-				low=-np.sqrt(6.0 / (n_in + n_out)),
-				high=np.sqrt(6.0 / (n_in + n_out)),
-				size=(n_out, n_in)), dtype=np.float64)
-		self.b = np.zeros(n_out, dtype=np.float64).reshape((n_out, 1))
-		self.n_in = n_in
-		self.n_out = n_out
+				low=-np.sqrt(6.0 / (num_inputs + num_outputs)),
+				high=np.sqrt(6.0 / (num_inputs + num_outputs)),
+				size=(num_outputs, num_inputs)), dtype=np.float64)
+		self.b = np.zeros(num_outputs, dtype=np.float64).reshape((num_outputs, 1))
+		self.num_inputs = num_inputs
+		self.num_outputs = num_outputs
 		self.activation = activation
 		self.d_activation = d_activation
 		self.next_layer = next_layer
 		self.alpha = alpha
 		self.lambda_penalty = lambda_penalty
+
+	def _backpropagate(self, x):
+		delta_hidden = self.next_layer.get_delta()
+		num_obs = x.shape[1]
+		self.W -= self.alpha * ((np.dot(delta_hidden, x.T) / num_obs) + self.lambda_penalty * self.W)
+		self.b -= self.alpha * (np.sum(delta_hidden, axis=1).reshape(self.num_outputs, 1) / num_obs)
 
 	def feed_forward(self, x, backprop):
 		num_obs = x.shape[1]
-		z = np.dot(self.W, x) + np.tile(self.b, (1, num_obs))
-		a_out = self.activation(z)
-		self.loss = self.lambda_penalty * (self.W ** 2).sum()
-		self.next_layer.feed_forward(a_out, backprop)
+		z1 = np.dot(self.W, x) + np.tile(self.b, (1, num_obs))
+		a1 = self.activation(z1)
+		self.loss = self.lambda_penalty / 2 * (self.W ** 2).sum()
+		self.next_layer.feed_forward(a1, backprop)
 		if backprop:
 			self._backpropagate(x)
 
-	def _backpropagate(self, x):
-		delta = self.next_layer.get_delta()
-		num_obs = x.shape[1]
-		self.W -= self.alpha * np.dot(delta, x.T) / num_obs
-		self.b -= self.alpha * np.sum(delta, axis=1).reshape(self.n_out, 1) / num_obs
-
 
 class Hidden_Layer(object):
-	def __init__(self, n_in, n_out, activation, d_activation, next_layer, alpha=0.1, lambda_penalty=0.0001, random_state=None):
+	"""
+	Hidden layer of multilayer perceptron.
+	Implements a softmax activation function for classification, however
+	paramter activation refers to the activation function used between
+	input and hidden layers.
+	"""
+	def __init__(self, num_inputs, num_outputs, activation, d_activation, next_layer,
+			alpha=0.1, lambda_penalty=0.0001, random_state=None):
+		# initialize weights and biases based on deep learning book recommendation
 		self.W = np.asarray(random_state.uniform(
-				low=-np.sqrt(6.0 / (n_in + n_out)),
-				high=np.sqrt(6.0 / (n_in + n_out)),
-				size=(n_out, n_in)), dtype=np.float64) * 4.0
-		self.b = np.zeros(n_out, dtype=np.float64).reshape((n_out, 1))
-		self.n_in = n_in
-		self.n_out = n_out
+				low=-np.sqrt(6.0 / (num_inputs + num_outputs)),
+				high=np.sqrt(6.0 / (num_inputs + num_outputs)),
+				size=(num_outputs, num_inputs)), dtype=np.float64) * 4.0
+		self.b = np.zeros(num_outputs, dtype=np.float64).reshape((num_outputs, 1))
+		self.num_inputs = num_inputs
+		self.num_outputs = num_outputs
 		self.activation = activation
 		self.d_activation = d_activation
 		self.next_layer = next_layer
 		self.alpha = alpha
 		self.lambda_penalty = lambda_penalty
+		self.loss = None
 
 	def get_delta(self):
 		"""
@@ -69,34 +79,31 @@ class Hidden_Layer(object):
 		"""
 		return self.delta
 
-	def _backpropagate(self, a_in):
-		num_obs = a_in.shape[1]
+	def _backpropagate(self, a1):
+		num_obs = a1.shape[1]
 		delta = self.next_layer.get_delta()
-		self.W -= self.alpha * np.dot(delta, a_in.T) / num_obs
-		self.b -= self.alpha * np.sum(delta, axis=1).reshape((self.n_out, 1)) / num_obs
-		self.delta = np.dot(self.W.T, delta) * self.d_activation(a_in)
+		self.W -= self.alpha * ((np.dot(delta, a1.T) / num_obs) + self.lambda_penalty * self.W)
+		self.b -= self.alpha * (np.sum(delta, axis=1).reshape((self.num_outputs, 1)) / num_obs)
+		self.delta = np.dot(self.W.T, delta) * self.d_activation(a1)
 
-	def feed_forward(self, a_in, backprop):
-		num_obs = a_in.shape[1]
-		z = np.dot(self.W, a_in) + np.tile(self.b, (1, num_obs))
-		a_out = self.activation(z)
-		self.loss = self.lambda_penalty * (self.W ** 2).sum()
-		self.next_layer.feed_forward(a_out, backprop)
+	def feed_forward(self, a1, backprop):
+		num_obs = a1.shape[1]
+		z2 = np.dot(self.W, a1) + np.tile(self.b, (1, num_obs))
+		exp_z = np.exp(z2)
+		a2 = exp_z / np.sum(exp_z, axis=0, keepdims=True)
+		self.loss = self.lambda_penalty / 2 * (self.W ** 2).sum()
+		self.next_layer.feed_forward(a2, backprop)
 		if backprop:
-			self._backpropagate(a_in)
+			self._backpropagate(a1)
 
 
 class Output_Layer(object):
-	def __init__(self, n_in, activation, d_activation, compute_error, compute_loss):
-		self.n_in = n_in
-		self.activation = activation
-		self.d_activation = d_activation
-		self.compute_error = compute_error
-		self.compute_loss = compute_loss
+	def __init__(self, num_inputs):
+		self.num_inputs = num_inputs
 
 	def set_y(self, y):
 		"""
-		initialize target values at output layer
+		initialize target values at output layer.
 		:param y:
 		:return:
 		"""
@@ -104,7 +111,7 @@ class Output_Layer(object):
 
 	def get_delta(self):
 		"""
-		pass delta for gradient descent to hidden layers
+		pass delta for gradient descent to hidden layers.
 		:return:
 		"""
 		return self.delta
@@ -112,12 +119,14 @@ class Output_Layer(object):
 	def get_yhat(self):
 		return self.yhat
 
-	def _backpropagate(self, a_in):
-		num_obs = a_in.shape[1]
-		self.delta = self.compute_error(self.y, a_in)
-		self.loss = self.compute_loss(self.y, a_in) / num_obs
+	def _backpropagate(self, a2):
+		num_obs = a2.shape[1]
+		self.loss = -1.0 * (np.sum(np.multiply(self.y, np.log(a2)))) / num_obs
+		# softmax:
+		a2[self.y[1, :], range(num_obs)] -= 1
+		self.delta = a2
 
-	def feed_forward(self, a_in, backprop):
-		self.yhat = a_in
+	def feed_forward(self, a2, backprop):
+		self.yhat = a2
 		if backprop:
-			self._backpropagate(a_in)
+			self._backpropagate(a2)

MLP.py
@@ -26,30 +26,33 @@ class MLP(object):
 		else:
 			random_state = np.random.RandomState(123)
 
-		# pass the loss and error functions to use
-		compute_error = lambda yhat, y: np.multiply(y - yhat, d_activation(y))
-		compute_loss = lambda yhat, y: 0.5 * np.sum((yhat - y) ** 2)
-
+		# define the activation function used between input and hidden layers
 		if self.activation == "sigmoid":
-			activation = sigmoid
-			d_activation = d_sigmoid
+			activation_func = sigmoid
+			d_activation_func = d_sigmoid
 		elif self.activation == "tanh":
-			activation = np.tanh
-			d_activation = d_tanh
+			activation_func = np.tanh
+			d_activation_func = d_tanh
 
 		num_layers = len(architecture)
 		for i in range(num_layers)[::-1]:
 			if i == num_layers - 1:
 				# output layer
-				self.layers.append(Output_Layer(architecture[i], activation, d_activation, compute_error=compute_error, compute_loss=compute_loss))
+				self.layers.append(Output_Layer(num_inputs=architecture[i]))
 			elif i == 0:
 				# input layer
-				self.layers.append(Input_Layer(architecture[i], architecture[i + 1], activation, d_activation, self.layers[-1], alpha=self.alpha, lambda_penalty=self.lambda_penalty, random_state=random_state))
+				self.layers.append(Input_Layer(num_inputs=architecture[i], num_outputs=architecture[i + 1],
+					activation=activation_func, d_activation=d_activation_func,
+					next_layer=self.layers[-1], alpha=self.alpha, lambda_penalty=self.lambda_penalty,
+					random_state=random_state))
 			else:
 				# hidden layer(s)
-				self.layers.append(Hidden_Layer(architecture[i], architecture[i + 1], activation, d_activation, self.layers[-1], alpha=self.alpha, lambda_penalty=self.lambda_penalty, random_state=random_state))
+				self.layers.append(Hidden_Layer(num_inputs=architecture[i], num_outputs=architecture[i + 1],
+					activation=activation_func, d_activation=d_activation_func,
+					next_layer=self.layers[-1], alpha=self.alpha, lambda_penalty=self.lambda_penalty,
+					random_state=random_state))
 
-	def fit(self, x, y, epochs, batch_size=None, pct_validation=.25):
+	def fit(self, x, y, epochs, batch_size, stall_limit=None, pct_validation=0.1):
 		# initialize network architecture
 		architecture = [x.shape[0]] + self.num_hidden_units + [y.shape[0]]
 		self._initialize_layers(architecture)
@@ -57,15 +60,14 @@ class MLP(object):
 		# split out a validation set if requested
 		if pct_validation <= 0 or pct_validation >= 1:
 			print "pct_validation must be between (0,1), resorting to default"
-			pct_validation = .25
-
+			pct_validation = 0.1
 		splits = split_validation(y, pct_validation)
 		x_train = x[:, splits['train']]
 		y_train = y[:, splits['train']]
 		x_valid = x[:, splits['validation']]
 		y_valid = y[:, splits['validation']]
 
-		# determine number of batches to run for each epoch (if SGD requested)
+		# determine number of batches to run for each epoch (if mini-batch requested)
 		num_obs = x_train.shape[1]
 		if batch_size is None:
 			# simply define batch size to be the whole set
@@ -76,9 +78,9 @@ class MLP(object):
 		# begin training
 		previous_score = -np.inf
 		stall_count = 0
-		stall_limit = int(math.floor(epochs/10))
+		stall_limit = int(math.floor(epochs/10)) if stall_limit is None else stall_limit
 		for i in range(epochs):
-			# if SGD requested, select a random subset of points to train on
+			# if mini-batch requested, select a subset of points to train on
 			for batch in range(num_batches):
 				start = batch * batch_size
 				end = min(num_obs, (batch + 1) * batch_size)
@@ -88,7 +90,7 @@ class MLP(object):
 			logging.info("Epoch %d:" % i)
 			logging.info("Loss = %f" % loss)
 			logging.info("Train Accuracy = %f" % train_score)
-			logging.info("Validation Accuracy = %f\n" % valid_score)
+			logging.info("Valid Accuracy = %f\n" % valid_score)
 
 			score_dif = valid_score - previous_score
 			if score_dif < 0.01:
@@ -101,7 +103,7 @@ class MLP(object):
 				logging.info("Epoch %d:" % i)
 				logging.info("Loss = %f" % loss)
 				logging.info("Train Accuracy = %f" % train_score)
-				logging.info("Validation Accuracy = %f\n" % valid_score)
+				logging.info("Valid Accuracy = %f\n" % valid_score)
 				break
 
 	def _fit_partial(self, x, y):

helper_funcs.py
@@ -4,7 +4,7 @@ import random
 import math
 
 
-def split_validation(y, pct_validation):
+def split_validation(y, pct_validation, random_seed):
 	"""
 	Statified sampling of indices to create equal representation of y in each split
 	:param y:
@@ -74,7 +74,7 @@ def d_sigmoid(x):
 
 def d_tanh(x):
 	# derivative of tanh activation function
-	return np.ones(x.shape) - (x ** 2)
+	return np.ones(x.shape) - np.power(x, 2)
 
 def squared_error(y, est):
 	num_classes = y.shape[1]

run_mlp.py
@@ -2,6 +2,7 @@ from __future__ import division
 import pandas as pd
 import numpy as np
 from sklearn.cross_validation import train_test_split
+from sklearn.preprocessing import StandardScaler
 from MLP import MLP
 import logging
 
@@ -12,6 +13,11 @@ x = df[df.columns[3:28]].values
 y = df[df.columns[2]].values
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.33, random_state=2015)
 
+# Normalize data
+scaler = StandardScaler()
+x_train = scaler.fit_transform(x_train)
+x_test = scaler.transform(x_test)
+
 # prepare data: transpose x's and y's, and binary-ize the y's
 x_train = x_train.T
 x_test = x_test.T
@@ -19,16 +25,15 @@ y_train = np.vstack((1 - y_train, y_train))
 y_test = np.vstack((1 - y_test, y_test))
 
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(filename)s[line:%(lineno)d] %(levelname)s %(message)s', datefmt='%a, %d %b %Y %H:%M:%S')
-
-mlp = MLP(num_hidden_units=300, alpha=.05, lambda_penalty=0.0001, activation="sigmoid", random_seed=1234)
-mlp.fit(x_train, y_train, epochs=5000, batch_size=90000, pct_validation=.1)
+mlp = MLP(num_hidden_units=50, alpha=.01, lambda_penalty=0.0001, activation="tanh", random_seed=1234)
+mlp.fit(x_train, y_train, epochs=50, batch_size=10000, stall_limit=100, pct_validation=0.1)
 
 probs_train = mlp.predict_proba(x_train)
-loss = np.mean( (probs_train - y_train)**2)
+loss = np.mean((probs_train - y_train)**2)
 print "Training Loss: ", loss
 
 probs_test = mlp.predict_proba(x_test)
-loss = np.mean( (probs_test - y_test)**2)
+loss = np.mean((probs_test - y_test)**2)
 print "Test Loss: ", loss
 
 class_scores = mlp.score_classes(x_train, y_train)